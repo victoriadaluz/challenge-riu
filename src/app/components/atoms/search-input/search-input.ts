69f0af13d49f4d4eedd481d5dba08fa4dad64900
victoriadaluz <vicky.daluz@gmail.com>
2026-03-11 13:52:54 -0300

    added search-input html

--- src/app/components/atoms/search-input/search-input.ts
+++ src/app/components/atoms/search-input/search-input.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, output, model} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-input.html',
   styleUrl: './search-input.css',
+  standalone: true,
 })
-export class SearchInput {}
+export class SearchInput {
+  placeholder = input<string>('');
+  ariaLabel = input<string>('');
+  value = model<string>('')
+
+  searched = output<string>();
+
+  protected query = '';
+  onSearch(): void {
+    this.searched.emit(this.value());
+  }
+
+}
