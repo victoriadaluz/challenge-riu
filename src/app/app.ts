c969659e49e59c41d0755322785ca84e2c25d3d9
victoriadaluz <vicky.daluz@gmail.com>
2026-03-11 11:57:57 -0300

    added featured article to home

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { FeaturedArticle } from './components/organisms/featured-article/featured-article';
 import { Button } from './components/atoms/button/button';
 import { SearchInput } from './components/atoms/search-input/search-input';
+import { Home } from './pages/home/home';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FeaturedArticle, Button, SearchInput],
+  imports: [RouterOutlet, FeaturedArticle, Button, SearchInput, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
