c68dc3d03f2dc1879345b724cc7b652bfacebeb8
victoriadaluz <vicky.daluz@gmail.com>
2026-03-11 12:06:25 -0300

    modified routes and global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FeaturedArticle } from './components/organisms/featured-article/featured-article';
-import { Button } from './components/atoms/button/button';
-import { SearchInput } from './components/atoms/search-input/search-input';
-import { Home } from './pages/home/home';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FeaturedArticle, Button, SearchInput, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
