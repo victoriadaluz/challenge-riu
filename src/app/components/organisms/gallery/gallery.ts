c85c97d6dd3671cee24b28d719531086a2eb96a9
victoriadaluz <victoriadaluz@gmail.com>
2026-03-12 18:39:45 -0300

    added header section

--- src/app/components/organisms/gallery/gallery.ts
+++ src/app/components/organisms/gallery/gallery.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, input} from '@angular/core';
+import { GalleryItem } from '../../../core/models/gallery-item.model';
 @Component({
   selector: 'app-gallery',
   imports: [],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
-export class Gallery {}
+export class Gallery {
+  sectionMainTitle = input<string>('');
+  sectionDescription = input<string>('');
+  items = input<GalleryItem[]>([]);
+}
