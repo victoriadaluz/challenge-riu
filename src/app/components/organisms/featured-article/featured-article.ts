1742861236539e2a5bd19181a902756ac034523e
victoriadaluz <vicky.daluz@gmail.com>
2026-03-15 12:41:51 -0300

    added text length and modified styles for responsive text wrap

--- src/app/components/organisms/featured-article/featured-article.ts
+++ src/app/components/organisms/featured-article/featured-article.ts
@@ -10,7 +10,9 @@ export class FeaturedArticle {
   date = input<string>('');
   datetime = input<string>('');
   title = input<string>('Lorem ipsum dolor sit amet.');
-  description = input<string>('Lorem impsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, at condimentum velit efficitur a. Sed ac felis eget nunc efficitur varius.');
+  description = input<string>(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.',
+  );
   imageUrl = input<string>('');
   imageAlt = input<string>('Lorem impsum');
   ctaLabel = input<string>('Lorem ipsum');
